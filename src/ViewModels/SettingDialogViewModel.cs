a70b1b2966005c24baeb405f40277c8a5c28c295
kaminaka rikuya <rikupin1105@gmail.com>
2022-07-31 00:45:43 +0900

    SettingDialogをコンボボックスに

--- src/ViewModels/SettingDialogViewModel.cs
+++ src/ViewModels/SettingDialogViewModel.cs
@@ -16,11 +16,12 @@ namespace SportsCoderForVolleyball.ViewModels
         {
             SubmitCommand.Subscribe(_ =>
             {
-                Control.Instance.POINT.Value = Point.Value;
-                Control.Instance.SET.Value = Set.Value;
-                Control.Instance.NEEDSET.Value = (Set.Value - 1) /2 + 1;
-                Control.Instance.LASTSETPOINT.Value = LastSetPoint.Value;
+                Control.Instance.POINT.Value = int.Parse(ComboPoint.Value);
+                Control.Instance.SET.Value = int.Parse(ComboSet.Value);
+                Control.Instance.NEEDSET.Value = (int.Parse(ComboSet.Value) - 1) /2 + 1;
+                Control.Instance.LASTSETPOINT.Value = int.Parse(ComboLastSet.Value);
                 Control.Instance.COURTCHANGE.Value = CourtChange.Value;
+
                 Control.Instance.TIMEOUT.Value = TimeOut.Value;
 
                 Control.Instance.TeamLeft.Value = ATeamName.Value;
@@ -31,7 +32,7 @@ namespace SportsCoderForVolleyball.ViewModels
 
                 Control.Instance.BackGroundColor.Value = BackGroundColor.Value;
 
-                if(Server.Value == "左")
+                if (Server.Value == "左")
                 {
                     Control.Instance.IsLeftServe.Value = true;
                     Control.Instance.IsRightServe.Value = false;
@@ -56,8 +57,8 @@ namespace SportsCoderForVolleyball.ViewModels
         public ReactiveProperty<string> ATeamName { get; set; } = Control.Instance.TeamLeft.ObserveProperty(x => x.Value).ToReactiveProperty();
         public ReactiveProperty<string> BTeamName { get; set; } = Control.Instance.TeamRight.ObserveProperty(x => x.Value).ToReactiveProperty();
         public ReactiveProperty<int> Set { get; set; } = Control.Instance.SET.ObserveProperty(x => x.Value).ToReactiveProperty();
-        public ReactiveProperty<int> Point { get; set; } = Control.Instance.POINT.ObserveProperty(x => x.Value).ToReactiveProperty();
-        public ReactiveProperty<int> LastSetPoint { get; set; } = Control.Instance.LASTSETPOINT.ObserveProperty(x => x.Value).ToReactiveProperty();
+        //public ReactiveProperty<int> Point { get; set; } = Control.Instance.POINT.ObserveProperty(x => x.Value).ToReactiveProperty();
+        //public ReactiveProperty<int> LastSetPoint { get; set; } = Control.Instance.LASTSETPOINT.ObserveProperty(x => x.Value).ToReactiveProperty();
         public ReactiveProperty<bool> CourtChange { get; set; } = Control.Instance.COURTCHANGE.ObserveProperty(x => x.Value).ToReactiveProperty();
         public ReactiveProperty<int> TimeOut { get; set; } = Control.Instance.TIMEOUT.ObserveProperty(x => x.Value).ToReactiveProperty();
 
@@ -71,13 +72,25 @@ namespace SportsCoderForVolleyball.ViewModels
         public ReactiveProperty<string> Server { get; set; } = new();
         public ReactiveProperty<int> ServerIndex { get; set; } = new();
 
+        public ReactiveProperty<string> ComboSet { get; set; } = new();
+        public ReactiveProperty<int> ComboSetIndex { get; set; } = new();
+
+        public ReactiveProperty<string> ComboPoint { get; set; } = new();
+        public ReactiveProperty<int> ComboPointIndex { get; set; } = new();
+
+        public ReactiveProperty<string> ComboLastSet { get; set; } = new();
+        public ReactiveProperty<int> ComboLastSetIndex { get; set; } = new();
+
+        public ReactiveProperty<string> ComboTimeOut { get; set; } = new();
+        public ReactiveProperty<int> ComboTimeOutIndex { get; set; } = new();
+
 
         public string Title => "設定";
 
         public event Action<IDialogResult> RequestClose;
 
         public bool CanCloseDialog() => true;
-        public void OnDialogClosed() 
+        public void OnDialogClosed()
         {
             Control.LockControl(true);
         }
@@ -85,6 +98,7 @@ namespace SportsCoderForVolleyball.ViewModels
         public void OnDialogOpened(IDialogParameters parameters)
         {
             Control.LockControl(false);
+
             if (Control.Instance.IsLeftFirstServe.Value)
             {
                 ServerIndex.Value = 0;
@@ -93,6 +107,40 @@ namespace SportsCoderForVolleyball.ViewModels
             {
                 ServerIndex.Value = 1;
             }
+
+            ComboSetIndex.Value = Control.Instance.SET.Value switch
+            {
+                7 => 0,
+                5 => 1,
+                3 => 2,
+                1 => 3,
+                _ => 1,
+            };
+
+            ComboPointIndex.Value = Control.Instance.POINT.Value switch
+            {
+                25 => 0,
+                21 => 1,
+                15 => 2,
+                _ => 1,
+            };
+
+            ComboLastSetIndex.Value = Control.Instance.LASTSETPOINT.Value switch
+            {
+                25 => 0,
+                21 => 1,
+                15 => 2,
+                _ => 2,
+            };
+
+            ComboTimeOutIndex.Value = Control.Instance.TIMEOUT.Value switch
+            {
+                0 => 0,
+                1 => 1,
+                2 => 2,
+                3 => 3,
+                _ => 2,
+            };
         }
     }
 }
