dededfc83035bed2fecfa665735e3a61b521b5c3
kaminaka rikuya <rikupin1105@gmail.com>
2023-08-09 16:23:42 +0900

    PointParSetinfomationSourceを移動

--- src/SportsCoderForVolleyball/Models/Control.cs
+++ src/SportsCoderForVolleyball/Models/Control.cs
@@ -16,22 +16,6 @@ namespace SportsCoderForVolleyball.Models
         {
             _dialogService = dialogService;
         }
-        public class PointParSetInfomationSource
-        {
-            public int Left { get; set; }
-            public int Right { get; set; }
-            public bool IsLeftWin
-            {
-                get
-                {
-                    if (Left>Right)
-                        return true;
-                    else
-                        return false;
-                }
-            }
-            public bool IsRightWin { get => !IsLeftWin; }
-        }
         public static void LockControl(bool look = true)
         {
             Instance.GuiEnable.Value = look;
---
+++ src/SportsCoderForVolleyball/Models/PointParSetInfomationSource.cs
@@ -0,0 +1,22 @@
+﻿namespace SportsCoderForVolleyball.Models
+{
+    public partial class Control
+    {
+        public class PointParSetInfomationSource
+        {
+            public int Left { get; set; }
+            public int Right { get; set; }
+            public bool IsLeftWin
+            {
+                get
+                {
+                    if (Left>Right)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+            public bool IsRightWin { get => !IsLeftWin; }
+        }
+    }
+}
