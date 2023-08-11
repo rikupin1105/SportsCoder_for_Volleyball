9532b40172415e43e7d088c7fac5c662670b4b8e
kaminaka rikuya <rikupin1105@gmail.com>
2023-08-11 23:43:27 +0900

    メッセージを追加

---
+++ src/SportsCoderForVolleyball.VerticalLeft/Views/Message.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SportsCoderForVolleyball.VerticalLeft.Views
+{
+    /// <summary>
+    /// Interaction logic for Message
+    /// </summary>
+    public partial class Message : UserControl
+    {
+        public Message()
+        {
+            InitializeComponent();
+        }
+    }
+}
